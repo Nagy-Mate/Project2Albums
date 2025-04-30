d9b4ddda82383a11b1f37315bbe87d3d5a9c628a
Nagy Máté <b1nagmat@vasvari.org>
2025-04-30 08:59:56 +0200

    api

--- Api/app.js
+++ Api/app.js
@@ -7,10 +7,63 @@ app.use(express.json());
 app.use(cors());
  
 app.get('/album', async (req, res)=>{
-    const albums = await dbAll("SELECT * FROM timetable");
+    const albums = await dbAll("SELECT * FROM albums");
     res.status(200).json(albums);
 });
 
+app.get('/album/:id', async (req,res) => {
+    const id = req.params.id;
+    const album = await dbGet("SELECT * FROM albums WHERE id = ?;", [id]);
+    if(!album){
+        return res.status(404).json({message: "Not found"});
+    }
+    res.status(200).json(album);
+    
+})
+
+app.post('/album', async (req, res) =>{
+    const {band, title, releaseDate, genre} = req.body;
+    if(!band || !title || !releaseDate || !genre ){
+        return res.status(400).json({message: "Missing data"});
+    }
+    const result = await dbRun("INSERT INTO albums (band, title, releaseDate, genre) VALUES (?, ?, ?, ?);",[band, title, releaseDate, genre]);
+    res.status(201).json({id: result.lastID, band, title, releaseDate, genre});
+});
+
+app.put("/album/:id", async (req, res) =>{
+    const id = req.params.id;
+    const data = await dbGet("SELECT * FROM albums WHERE id = ?;", [id]);
+
+    if(data.length === 0){
+        return res.status(404).json({message: "Not found"});
+    }
+
+    const {band, title, releaseDate, genre} = req.body;
+
+    if(!band || !title || !releaseDate || !genre ){
+        return res.status(400).json({message: "Missing data"});
+    }
+
+     try {
+        // Frissítjük az adatbázist
+        await dbRun("UPDATE albums SET band = ?, title = ?, releaseDate = ?, genre = ? WHERE id = ?;", [band, title, releaseDate, genre, id]);
+        return res.status(200).json({ id, band, title, releaseDate, genre });
+    } catch (error) {
+        // Ha hiba történt az adatbázis műveletnél
+        console.error(error);
+        return res.status(500).json({ message: "Error updating album" });
+    }
+});
+
+app.delete("/album/:id", async (req, res)=>{
+    const id = req.params.id;
+    const data = dbGet("SELECT * FROM albums WHERE id = ?;", [id]);
+    if(!data){
+        return res.status(404).json({message: "Not found"});
+    }
+    await dbRun("DELETE FROM albums WHERE id = ?;", [id]);
+    res.status(200).json({message: "Successfuly deleted! "});
+});
 
 app.use((req, res, next, err) =>{
     if(err){
--- Api/util/database.js
+++ Api/util/database.js
@@ -32,21 +32,21 @@ export async function initializeDatabase(){
     await dbRun("DROP TABLE IF EXISTS albums");
     await dbRun("CREATE TABLE IF NOT EXISTS albums (id INTEGER PRIMARY KEY AUTOINCREMENT, band STRING, title STRING, releaseDate DATE, genre STRING)");
 
-    const timetable =[
-        ('Pink Floyd', 'The Dark Side of the Moon', '1973-03-01', 'Progressive Rock'),
-  ('Nirvana', 'Nevermind', '1991-09-24', 'Grunge'),
-  ('The Beatles', 'Abbey Road', '1969-09-26', 'Rock'),
-  ('Radiohead', 'OK Computer', '1997-05-21', 'Alternative Rock'),
-  ('Daft Punk', 'Discovery', '2001-03-12', 'Electronic'),
-  ('Metallica', 'Master of Puppets', '1986-03-03', 'Thrash Metal'),
-  ('Taylor Swift', '1989', '2014-10-27', 'Pop'),
-  ('Miles Davis', 'Kind of Blue', '1959-08-17', 'Jazz'),
-  ('Beyoncé', 'Lemonade', '2016-04-23', 'R&B'),
-  ('Kendrick Lamar', 'DAMN.', '2017-04-14', 'Hip-Hop')
+    const albums =[
+        {band: 'Pink Floyd', title:  'The Dark Side of the Moon',releaseDate:  '1973-03-01',genre: 'Progressive Rock'},
+        {band: 'Nirvana',title:  'Nevermind',releaseDate:  '1991-09-24',genre:  'Grunge'},
+        {band: 'The Beatles',title:  'Abbey Road',releaseDate:  '1969-09-26',genre:  'Rock'},
+        {band: 'Radiohead',title:  'OK Computer',releaseDate:  '1997-05-21',genre:  'Alternative Rock'},
+        {band: 'Daft Punk',title:  'Discovery',releaseDate:  '2001-03-12',genre:  'Electronic'},
+        {band: 'Metallica',title:  'Master of Puppets',releaseDate:  '1986-03-03',genre:  'Thrash Metal'},
+        {band: 'Taylor Swift',title:  '1989',releaseDate:  '2014-10-27',genre:  'Pop'},
+        {band: 'Miles Davis',title:  'Kind of Blue',releaseDate:  '1959-08-17',genre:  'Jazz'},
+        {band: 'Beyoncé',title:  'Lemonade',releaseDate:  '2016-04-23',genre:  'R&B'},
+        {band: 'Kendrick Lamar',title:  'DAMN.',releaseDate:  '2017-04-14',genre:  'Hip-Hop'}
 
     ];
 
-    for(const t of timetable){
-        await dbRun("INSERT INTO timetable (day, time, subject) VALUES (?, ?, ?);",[t.day, t.time, t.subject]);
+    for(const album of albums){
+        await dbRun("INSERT INTO albums (band, title, releaseDate, genre) VALUES (?, ?, ?, ?);",[album.band, album.title, album.releaseDate,album.genre]);
     } 
 }
